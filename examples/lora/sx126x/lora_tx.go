d393d4c0f48fccad098604c7038b721c08511799
Olivier Fauchon <ofauchon2204@gmail.com>
2021-04-01 23:55:40 +0200

    WIP

--- examples/lora/sx126x/lora_tx.go
+++ examples/lora/sx126x/lora_tx.go
@@ -12,111 +12,117 @@ import (
 
 func SubGhzInit() error {
 
-	// TODO : VOS
+	// TODO : NVIC, VOS
 
-	// Init SubGhz SPI
+	// Enable APB3 Periph clock
 	stm32.RCC.APB3ENR.SetBits(stm32.RCC_APB3ENR_SUBGHZSPIEN)
-	_ = stm32.RCC.APB3ENR.Get() // Delay after RCC periph clock enable
+	time.Sleep(time.Millisecond) //Delay after RCC periph clock enable
 
 	// Enable TXCO and HSE
-	// Already done in runtime_stm32wl .... FIXME
 	stm32.RCC.CR.SetBits(stm32.RCC_CR_HSEBYPPWR)
 	stm32.RCC.CR.SetBits(stm32.RCC_CR_HSEON)
 	for !stm32.RCC.CR.HasBits(stm32.RCC_CR_HSERDY) {
 	}
 
-	// Reset SUBGHZ device
-	stm32.RCC.CSR.SetBits(stm32.RCC_CSR_RFRST)
-	time.Sleep(time.Millisecond * 10)
+	// Disable radio reset and wait it's ready
 	stm32.RCC.CSR.ClearBits(stm32.RCC_CSR_RFRST)
 	for stm32.RCC.CSR.HasBits(stm32.RCC_CSR_RFRSTF) {
 	}
 
-	// Disable radio : SUBGHZ SPI NSS signal is at level high (RM0461-5.5.18)
+	// Disable radio NSS=1
 	stm32.PWR.SUBGHZSPICR.SetBits(stm32.PWR_SUBGHZSPICR_NSS)
 
 	// Enable Exti Line 44: Radio IRQ ITs for CPU1 (RM0461-14.3.1)
 	stm32.EXTI.IMR2.SetBits(1 << 12) // IM44
 
+	// Enable radio busy wakeup from Standby for CPU
+	// Is it required ?
+	stm32.PWR.CR3.SetBits(stm32.PWR_CR3_EWRFBUSY)
+
 	// Clear busy flag
 	stm32.PWR.SCR.SetBits(stm32.PWR_SCR_CWRFBUSYF)
 
-	// Enable radio busy wakeup from Standby for CPU (RM0461-5.5.3)
-	//stm32.PWR.CR3.SetBits(stm32.PWR_CR3_EWRFBUSY)
+	//  SUBGHZSPI configuration
+	stm32.SPI3.CR1.ClearBits(stm32.SPI_CR1_SPE)                                                   // Disable SPI
+	stm32.SPI3.CR1.Set(stm32.SPI_CR1_MSTR | stm32.SPI_CR1_SSI | (0b010 << 3) | stm32.SPI_CR1_SSM) // Software Slave Management (NSS) + /8 prescaler
+	//stm32.SPI3.CR1.Set(stm32.SPI_CR1_MSTR | stm32.SPI_CR1_SSI | (0b110 << 3) | stm32.SPI_CR1_SSM) // Software Slave Management (NSS) + /64 prescaler
+	stm32.SPI3.CR2.Set(stm32.SPI_CR2_FRXTH | (0b111 << 8)) // FIFO Threshold and 8bit size
+	stm32.SPI3.CR1.SetBits(stm32.SPI_CR1_SPE)              // Enable SPI
 
-	// Clear wakeup radio busy flag (RM0461-5.5.7)
-	//stm32.PWR.SCR.SetBits(stm32.PWR_SCR_CWRFBUSYF)
-
-	//  SUBGHZSPI configuration (Prescaler /8)
-	stm32.SPI3.CR1.ClearBits(stm32.SPI_CR1_SPE)                                                  // Disable SPI
-	stm32.SPI3.CR1.Set(stm32.SPI_CR1_MSTR | stm32.SPI_CR1_SSI | (0b10 << 3) | stm32.SPI_CR1_SSM) // Software Slave Management (NSS)
-	stm32.SPI3.CR2.Set(stm32.SPI_CR2_FRXTH | (0b111 << 8))                                       // FIFO Threshold and 8bit size
-	stm32.SPI3.CR1.SetBits(stm32.SPI_CR1_SPE)                                                    // Enable SPI
-
+	println("CR1", stm32.SPI3.CR1.Get())
+	println("CR2", stm32.SPI3.CR2.Get())
 	return nil
-}
-
-func xstatus(lora sx126x.Device) {
-	dat := lora.GetStatus()
-	chipmode := (dat & (0x7 << 4)) >> 4
-	cmdstatus := (dat & (0x7 << 1)) >> 1
-	println("mode:", chipmode, " cmd:", cmdstatus, " irq:", dat)
 }
 
 // DS.SX1261-2.W.APP section 14.2 p99
 func configureLora(lora sx126x.Device) {
 
-	// Standby mode for configuration
-	lora.Standby()
+	// Force Standby mode for configuration
+	// Maybe we could replace with a check over current state
+	// and fire an error if not in standby mode ?
+	lora.SetStandby()
 
-	println("Clear IRQ")
-	// Clear Interrupt Flag
-	lora.ClearIrqStatus(sx126x.SX126X_IRQ_ALL)
+	println("SetBufferBaseAddress")
+	lora.SetBufferBaseAddress(0, 0)
 
 	println("SetPacketType")
-	// Define the protocol
 	lora.SetPacketType(sx126x.SX126X_PACKET_TYPE_LORA)
 
-	println("SetPaConfig")
-	// Set Power Amplifier
-	lora.SetPaConfig(0x04, 0x07, 0x00, 0x01)
+	// Here: Add fallback to STDBY_RC after RX/TX
+	// See: https://github.com/jgromes/RadioLib/blob/86ca714d0017419aebc5bae5b4e8010e71e66874/src/modules/SX126x/SX126x.cpp#L1550
 
-	println("SetTxParams")
-	// Set output power and ramping time (0x16:HP22Db)
-	lora.SetTxParams(0x16, sx126x.SX126X_PA_RAMP_200U) // CHECKME
+	// Here: Add CAD Configuration
+	// See: https://github.com/jgromes/RadioLib/blob/86ca714d0017419aebc5bae5b4e8010e71e66874/src/modules/SX126x/SX126x.cpp#L1561
 
-	println("SetBaseAddress")
-	// Were to store the payload in buffer
-	lora.SetBufferBaseAddress(0, 0)
+	println("Configure Dio Irq")
+	lora.ClearIrqStatus(sx126x.SX126X_IRQ_ALL)
+	lora.SetDioIrqParams(sx126x.SX126X_IRQ_NONE, sx126x.SX126X_IRQ_NONE, sx126x.SX126X_IRQ_NONE, sx126x.SX126X_IRQ_NONE)
+
+	println("Calibrate all blocks")
+	lora.CalibrateAll()
+	time.Sleep(10 * time.Millisecond) //(See Man sec.13.1.12)
+
+	// Here: Add optionnal TCXO configuration
+	// https://github.com/jgromes/RadioLib/blob/86ca714d0017419aebc5bae5b4e8010e71e66874/src/modules/SX126x/SX126x.cpp#L110
+
+	println("Limit current")
+	lora.SetCurrentLimit(60)
 
 	println("SetModulation")
 	// Set modulation params
 	// SF7 / 125 KHz / CR 4/7 / No Optimis
 	lora.SetModulationParams(7, sx126x.SX126X_LORA_BW_125_0, sx126x.SX126X_LORA_CR_4_7, sx126x.SX126X_LORA_LOW_DATA_RATE_OPTIMIZE_OFF)
 
+	println("SetPaConfig")
+	//	lora.SetPaConfig(0x04, 0x07, 0x00, 0x01)
+	lora.SetPaConfig(0x02, 0x02, 0x00, 0x01)
+
+	println("SetTxParams") //Power and Ramping Time
+	// Set output power and ramping time (0x16:HP22Db)
+	lora.SetTxParams(0x16, sx126x.SX126X_PA_RAMP_200U) // CHECKME
+
 	// Set Lora Sync Word
 	lora.SetLoraPublicNetwork(true)
 
-	// Set Sync Word (0x34?)
-	// TODO
-	/*
-		println("Start TX")
-		// Start transmission
-		lora.SetTx(sx126x.SX126X_TX_TIMEOUT_NONE)
+	// TODO: Set regulator DCDC/LDO
 
-		println("Wait IRQ")
-		// Wait IRQ TxDone or Timeout
+}
+
+func xstatus(lora sx126x.Device) {
+	dat := lora.GetStatus()
+	chipmode := (dat & (0x7 << 4)) >> 4
+	cmdstatus := (dat & (0x7 << 1)) >> 1
+	println("mode:", chipmode, " cmd:", cmdstatus, " irq:", dat, "deverr", lora.GetDeviceErrors())
+	lora.ClearDeviceErrors()
+}
+
+// Helper Error
+func checkErr(e error) {
+	if e != nil {
+		println("!!! Error:", e, " -> Stopping")
 		for {
-			st := lora.GetIrqStatus()
-			time.Sleep(1000 * time.Millisecond)
-			print("IRQ Status:", st)
 		}
-		println("TX DONE")
-
-		println("ClearIrqStatus")
-		// Clear Interrupt Flag
-		lora.ClearIrqStatus(sx126x.SX126X_IRQ_TX_DONE)
-	*/
+	}
 }
 
 func main() {
@@ -125,53 +131,116 @@ func main() {
 	led := machine.LED
 	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
-	// Configure UART0 (Console)
-	machine.UART0.Configure(machine.UARTConfig{TX: machine.UART_TX_PIN, RX: machine.UART_TX_PIN, BaudRate: 9600})
-
-	println("STM32WL Radio Init Example")
-
-	// Init radio module
-	SubGhzInit()
-
-	// Attach SPI driver
-	lora := sx126x.New(machine.SPI0)
-
-	// Switch Radio to Sleep, then Standby
-	lora.Sleep()
-	lora.Standby()
-	println("* Now in Standby")
-	xstatus(lora)
-
-	// Sets Lora configuration
-	configureLora(lora)
-
-	//msg := []byte("Hello TinyGo!")
-	msg := []byte{0x00, 0xDC, 0x00, 0x00, 0xD0, 0x7E, 0xD5, 0xB3, 0x70, 0x1E, 0x6F, 0xED, 0xF5, 0x7C, 0xEE, 0xAF, 0x00, 0x85, 0xCC, 0x58, 0x7F, 0xE9, 0x13}
-
-	// Switch to RX
-	lora.SetRx(sx126x.SX126X_RX_TIMEOUT_NONE)
-	time.Sleep(100 * time.Millisecond)
-
-	println("SetRfFrequency")
-	//Define the RF Frequency (Only when in RX/TX/FS mode )
-	lora.SetRfFrequency(868000000)
-
+	// This test : clock, gpio
 	for {
-
-		// write the payload
-		println("WriteBuffer")
-		lora.WriteBuffer(msg)
-
-		// Define frame format
-		//10 preamble bits, 8bits preamble detection,
-		println("SetPacketParam")
-		lora.SetPacketParam(10, 0x04, uint8(len(msg)), sx126x.SX126X_LORA_CRC_ON, sx126x.SX126X_LORA_IQ_STANDARD)
-
-		lora.SetTx(sx126x.SX126X_TX_TIMEOUT_NONE)
-
-		xstatus(lora)
-		time.Sleep(1000 * time.Millisecond)
-		xstatus(lora)
+		machine.LED.Set(true)
+		time.Sleep(time.Millisecond * 1000)
+		machine.LED.Set(false)
+		time.Sleep(time.Millisecond * 1000)
 	}
+
+	// Test UART 9600 baud
+	machine.UART0.Configure(machine.UARTConfig{TX: machine.UART_TX_PIN, RX: machine.UART_TX_PIN, BaudRate: 9600})
+	println("Lora_tx test")
+
+	/*
+
+
+		machine.LED.Set(true)
+		time.Sleep(time.Millisecond * 2000)
+		machine.LED.Set(false)
+
+		// Configure RF GPIO
+		rfswitchPB5 := machine.PB5
+		rfswitchPB5.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+		rfswitchPA4 := machine.PA4
+		rfswitchPA4.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+
+		println("STM32WL Radio Init Example")
+
+		// Init radio module
+		println("SubGhzInit")
+		SubGhzInit()
+
+		println("Init Driver")
+		lora := sx126x.New(machine.SPI0)
+		//xstatus(lora)
+
+			// Switch Radio to Sleep, then Standby
+			println("Go Sleep")
+			//lora.SetSleep()
+			println("Go Standby")
+			//lora.SetStandby()
+			println("* Now in Standby")
+
+			//xstatus(lora)
+
+		println("Read SyncWord Registers")
+
+		addr := uint16(0x0740)
+		rs, err := lora.ReadRegister(addr, 1)
+		println("~~~~~~~~~~~~~~", rs[0])
+				for i := 0; i < len(rs); i++ {
+					println("Sync reg ", fmt.Sprintf("%04x", int(addr)+i), fmt.Sprintf("%02x", rs[i]))
+
+				}
+			println("Sync reg 0x0741", rs[1])
+		checkErr(err)
+
+			xstatus(lora)
+
+			// Sets Lora configuration
+			configureLora(lora)
+
+			//msg := []byte("Hello TinyGo!")
+			msg := []byte{0x00, 0xDC, 0x00, 0x00, 0xD0, 0x7E, 0xD5, 0xB3, 0x70, 0x1E, 0x6F, 0xED, 0xF5, 0x7C, 0xEE, 0xAF, 0x00, 0x85, 0xCC, 0x58, 0x7F, 0xE9, 0x13}
+
+			// Switch to RX
+			lora.SetRx(sx126x.SX126X_RX_TIMEOUT_NONE)
+			time.Sleep(100 * time.Millisecond)
+
+			println("SetRfFrequency")
+			lora.SetRfFrequency(868000000) // Needs to be done in FS/RX/TX mode ?
+
+			println("SetPacketParam")
+			lora.SetPacketParam(10, 0x04, uint8(len(msg)), sx126x.SX126X_LORA_CRC_ON, sx126x.SX126X_LORA_IQ_STANDARD)
+
+			// Show current status
+			xstatus(lora)
+
+
+			for {
+
+				// ------------------------------> TX
+				// write the payload
+				println("Write TX packet")
+				lora.WriteBuffer(msg)
+
+				// Clear IRQ Flag
+				lora.ClearIrqStatus(0x0)
+
+				// Set RF Switch to TX
+				rfswitchPA4.Set(false)
+				rfswitchPB5.Set(true)
+
+				// Set TX
+				lora.SetTx(sx126x.SX126X_TX_TIMEOUT_NONE)
+
+				println("Wait 10s")
+
+				xstatus(lora)
+				time.Sleep(10000 * time.Millisecond)
+
+			}
+
+	*/
+
+	// Set RF Switch to TX
+	//rfswitchPA4.Set(false)
+	//rfswitchPB5.Set(true)
+
+	//lora.SetTxContinuousWave()
 
 }
--- lora/sx126x/sx126x.go
+++ lora/sx126x/sx126x.go
@@ -5,6 +5,7 @@ package sx126x
 import (
 	"device/stm32"
 	"errors"
+	"fmt"
 	"machine"
 	"time"
 )
@@ -26,6 +27,8 @@ type Device struct {
 	spi            machine.SPI
 	radioEventChan chan RadioEvent
 	cnf            Config
+
+	deepSleep bool
 }
 
 // Config holds the LoRa configuration parameters
@@ -36,13 +39,14 @@ type Config struct {
 func New(spi machine.SPI) Device {
 	k := Device{
 		spi:            spi,
+		deepSleep:      true,
 		radioEventChan: make(chan RadioEvent, 10),
 	}
 	return k
 }
 
 // --------------------------------------------------
-// Radio-specific functions
+// Get function (state, irq ...)
 // --------------------------------------------------
 
 // GetStatus returns radio status
@@ -51,10 +55,23 @@ func (d *Device) GetStatus() uint8 {
 	return r[0]
 }
 
-// GetPackeType returns radio status
+// GetPackeType returns current Packet Type
 func (d *Device) GetPacketType() uint8 {
 	r := d.ExecGetCommand(SX126X_CMD_GET_PACKET_TYPE, 1)
 	return r[0]
+}
+
+// GetDeviceErrors returns current Device Errors
+func (d *Device) GetDeviceErrors() uint16 {
+	r := d.ExecGetCommand(SX126X_CMD_GET_DEVICE_ERRORS, 2)
+	ret := uint16(r[0]<<8 + r[1])
+	return ret
+}
+
+// ClearDeviceErrors clears device Errors
+func (d *Device) ClearDeviceErrors() {
+	p := [2]uint8{0x00, 0x00}
+	d.ExecSetCommand(SX126X_CMD_CLEAR_DEVICE_ERRORS, p[:])
 }
 
 // GetIrqStatus returns radio status
@@ -62,6 +79,21 @@ func (d *Device) GetIrqStatus() uint16 {
 	r := d.ExecGetCommand(SX126X_CMD_GET_STATUS, 3)
 	ret := uint16(r[0]<<8 | r[1])
 	return ret
+}
+
+// -----------------------------------
+// STATE MANAGEMENT (RX/TX/STBY/SLEEP)
+// -----------------------------------
+
+// Sleep switch device to sleep mode
+func (d *Device) SetSleep() {
+	// Todo : Ant switch ? Warm start ?
+	d.ExecSetCommand(SX126X_CMD_SET_SLEEP, []uint8{SX126X_SLEEP_START_WARM | SX126X_SLEEP_RTC_OFF})
+}
+
+// Standby switch device to RC standby mode (R)
+func (d *Device) SetStandby() {
+	d.ExecSetCommand(SX126X_CMD_SET_STANDBY, []uint8{SX126X_STANDBY_RC})
 }
 
 // SetTx enable Tx Mode with Tx Timeout (R)
@@ -82,16 +114,14 @@ func (d *Device) SetRx(t uint32) {
 	d.ExecSetCommand(SX126X_CMD_SET_RX, p[:])
 }
 
-// Sleep switch device to sleep mode
-func (d *Device) Sleep() {
-	// Todo : Ant switch ? Warm start ?
-	d.ExecSetCommand(SX126X_CMD_SET_SLEEP, []uint8{SX126X_SLEEP_START_WARM | SX126X_SLEEP_RTC_OFF})
+// SetTxContinuousWave enable Rx Mode with Rx Timeout (R)
+func (d *Device) SetTxContinuousWave() {
+	d.ExecSetCommand(SX126X_CMD_SET_TX_CONTINUOUS_WAVE, []uint8{})
 }
 
-// Standby switch device to RC standby mode (R)
-func (d *Device) Standby() {
-	d.ExecSetCommand(SX126X_CMD_SET_STANDBY, []uint8{SX126X_STANDBY_RC})
-}
+// ---------------------------------------
+// PACKET / RADIO / PROTOCOL CONFIGURATION
+// ---------------------------------------
 
 // SetPacketType sets the packet type
 func (d *Device) SetPacketType(packetType uint8) {
@@ -114,7 +144,6 @@ func (d *Device) SetLoraPublicNetwork(enable bool) {
 		d.SetSyncWord(SX126X_LORA_MAC_PUBLIC_SYNCWORD)
 	} else {
 		d.SetSyncWord(SX126X_LORA_MAC_PRIVATE_SYNCWORD)
-
 	}
 }
 
@@ -150,10 +179,29 @@ func (d *Device) SetRfFrequency(frequency uint32) {
 	d.ExecSetCommand(SX126X_CMD_SET_RF_FREQUENCY, p[:])
 }
 
+// SetDioIrqParams configures DIO Irq
+func (d *Device) SetDioIrqParams(irqMask, dio1Mask, dio2Mask, dio3Mask uint16) {
+	var p [8]uint8
+	p[0] = uint8((irqMask >> 8) & 0xFF)
+	p[1] = uint8(irqMask & 0xFF)
+	p[2] = uint8((dio1Mask >> 8) & 0xFF)
+	p[3] = uint8(dio1Mask & 0xFF)
+	p[4] = uint8((dio2Mask >> 8) & 0xFF)
+	p[5] = uint8(dio2Mask & 0xFF)
+	p[6] = uint8((dio3Mask >> 8) & 0xFF)
+	p[7] = uint8(dio3Mask & 0xFF)
+	d.ExecSetCommand(SX126X_CMD_SET_DIO_IRQ_PARAMS, p[:])
+}
+
+// CalibrateAll
+func (d *Device) CalibrateAll() {
+	p := []uint8{SX126X_CALIBRATE_ALL}
+	d.ExecSetCommand(SX126X_CMD_CALIBRATE, p[:])
+}
+
+// CalibrateImage
 func (d *Device) CalibrateImage(freq uint32) {
-
 	var calFreq [2]uint8
-
 	if freq > 900000000 {
 		calFreq[0] = 0xE1
 		calFreq[1] = 0xE9
@@ -171,7 +219,6 @@ func (d *Device) CalibrateImage(freq uint32) {
 		calFreq[1] = 0x6F
 	}
 	d.ExecSetCommand(SX126X_CMD_CALIBRATE_IMAGE, calFreq[:])
-
 }
 
 // SetPaConfig sets the Power Amplifier configuration (R)
@@ -182,6 +229,16 @@ func (d *Device) SetPaConfig(paDutyCycle, hpMax, deviceSel, paLut uint8) {
 	p[2] = deviceSel
 	p[3] = paLut
 	d.ExecSetCommand(SX126X_CMD_SET_PA_CONFIG, p[:])
+}
+
+// SetCurrentLimit sets max current
+func (d *Device) SetCurrentLimit(limit uint8) {
+	if limit > 140 {
+		limit = 140
+	}
+	rawLimit := uint8(float32(limit) / 2.5)
+	p := []uint8{rawLimit}
+	d.WriteRegister(SX126X_REG_OCP_CONFIGURATION, p[:])
 }
 
 // SetTxConfig sets power and rampup time (R)
@@ -210,9 +267,10 @@ func (d *Device) ClearIrqStatus(clearIrqParams uint16) {
 	d.ExecSetCommand(SX126X_CMD_CLEAR_IRQ_STATUS, p[:])
 }
 
-// ***************
+// *******
 // BUFFERS
-// **************
+// *******
+
 // WriteBuffer sets base address for buffer
 func (d *Device) WriteBuffer(data []uint8) {
 	p := []uint8{0} // Zero offset
@@ -220,82 +278,86 @@ func (d *Device) WriteBuffer(data []uint8) {
 	d.ExecSetCommand(SX126X_CMD_WRITE_BUFFER, p[:])
 }
 
-// --------------------------------------------------
-// Internal functions
-// --------------------------------------------------
-
-// Reset re-initialize the sx127x device
-// sx1276 have nRST pin
-/*
-func (d *Device) Reset() {
-	d.rstPin.Low()
-	time.Sleep(100 * time.Millisecond)
-	d.rstPin.High()
-	time.Sleep(100 * time.Millisecond)
-}
-*/
-
-// ReadRegister reads register value
-func (d *Device) ReadRegister(addr, size uint16) []uint8 {
-	stm32.PWR.SUBGHZSPICR.ClearBits(stm32.PWR_SUBGHZSPICR_NSS)
-
-	buf := []byte{SX126X_CMD_READ_REGISTER, uint8((addr >> 8) & 0xFF), uint8(addr & 0xFF), 0x00} // Sec. 13.2.2
-	d.spi.Tx(buf, nil)
-
-	ret := make([]uint8, size)
-	for i := uint16(0); i < size; i++ {
-		d.spi.Tx([]uint8{0x00}, ret)
-	}
-	stm32.PWR.SUBGHZSPICR.SetBits(stm32.PWR_SUBGHZSPICR_NSS)
-	d.WaitBusy()
-	return ret
-}
-
 // CheckDeviceReady sleep until all busy flags clears
 func (d *Device) CheckDeviceReady() error {
-	// Wakeup radio in case of sleep mode: Select-Unselect radio
-	// In case of Deep Seep ????
-	stm32.PWR.SUBGHZSPICR.SetBits(stm32.PWR_SUBGHZSPICR_NSS)
-	time.Sleep(time.Millisecond)
-	stm32.PWR.SUBGHZSPICR.ClearBits(stm32.PWR_SUBGHZSPICR_NSS)
+	if d.deepSleep == true {
+		stm32.PWR.SUBGHZSPICR.ClearBits(stm32.PWR_SUBGHZSPICR_NSS)
+		time.Sleep(time.Millisecond)
+		stm32.PWR.SUBGHZSPICR.SetBits(stm32.PWR_SUBGHZSPICR_NSS)
+	}
 	return d.WaitBusy()
 }
 
 // WaitBusy sleep until all busy flags clears
 func (d *Device) WaitBusy() error {
-	count := 10000
-	// RM0461 Sec 5.3 Radio Busy Management
+	count := 10
+	var flag, mask bool
 	for count > 0 {
-		fl1 := stm32.PWR.SR2.HasBits(stm32.PWR_SR2_RFBUSYMS)
-		fl2 := stm32.PWR.SR2.HasBits(stm32.PWR_SR2_RFBUSYS)
-		if !fl1 && !fl2 {
+		mask = stm32.PWR.SR2.HasBits(stm32.PWR_SR2_RFBUSYMS)
+		flag = stm32.PWR.SR2.HasBits(stm32.PWR_SR2_RFBUSYS)
+
+		if !(flag && mask) {
 			return nil
 		}
+		time.Sleep(time.Millisecond)
 		count--
 	}
+	println("ERR: WaitBusy timeout.")
 	return errors.New("WaitBusy Timeout")
 }
 
+// ReadRegister reads register value
+func (d *Device) ReadRegister(addr, size uint16) ([]uint8, error) {
+	d.CheckDeviceReady()
+	stm32.PWR.SUBGHZSPICR.ClearBits(stm32.PWR_SUBGHZSPICR_NSS) // NSS=0
+
+	// Send command
+	d.SpiTx(SX126X_CMD_READ_REGISTER)
+	d.SpiTx(uint8((addr & 0xFF00) >> 8))
+	d.SpiTx(uint8(addr & 0x00FF))
+	d.SpiTx(uint8(0x00)) // This byte is for status (unused yet)
+
+	var ret []uint8
+	for i := 0; i < int(size); i++ {
+		b := d.SpiRx()
+		println("IN: ", b)
+		ret = append(ret, b)
+	}
+	stm32.PWR.SUBGHZSPICR.SetBits(stm32.PWR_SUBGHZSPICR_NSS) // NSS=1
+	d.WaitBusy()
+	return ret, nil
+}
+
 // WriteRegister writes value to register
-func (d *Device) WriteRegister(reg uint16, data []uint8) {
-	stm32.PWR.SUBGHZSPICR.ClearBits(stm32.PWR_SUBGHZSPICR_NSS)
-
-	buf := []byte{SX126X_CMD_WRITE_REGISTER, uint8((reg >> 8) & 0xFF), uint8(reg & 0xFF)}
-
-	d.spi.Tx(buf, nil)
-	d.spi.Tx(data, nil)
-
-	stm32.PWR.SUBGHZSPICR.SetBits(stm32.PWR_SUBGHZSPICR_NSS)
+func (d *Device) WriteRegister(addr uint16, data []uint8) {
+	d.CheckDeviceReady()
+	stm32.PWR.SUBGHZSPICR.ClearBits(stm32.PWR_SUBGHZSPICR_NSS) // NSS=0
+	// Send command
+	d.SpiTx(SX126X_CMD_WRITE_REGISTER)
+	d.SpiTx(uint8((addr & 0xFF00) >> 8))
+	d.SpiTx(uint8(addr & 0x00FF))
+	for i := 0; i < len(data); i++ {
+		d.SpiTx(data[i])
+	}
+	stm32.PWR.SUBGHZSPICR.SetBits(stm32.PWR_SUBGHZSPICR_NSS) // NSS=1
 	d.WaitBusy()
 }
 
 // ExecSetCommand send a command to configure the peripheral
 func (d *Device) ExecSetCommand(cmd uint8, buf []uint8) {
 	d.CheckDeviceReady()
-	stm32.PWR.SUBGHZSPICR.ClearBits(stm32.PWR_SUBGHZSPICR_NSS)
-	d.spi.Tx([]byte{cmd}, nil)
-	d.spi.Tx(buf, nil)
-	stm32.PWR.SUBGHZSPICR.SetBits(stm32.PWR_SUBGHZSPICR_NSS)
+	if cmd == SX126X_CMD_SET_SLEEP {
+		d.deepSleep = true
+	} else {
+		d.deepSleep = false
+	}
+	stm32.PWR.SUBGHZSPICR.ClearBits(stm32.PWR_SUBGHZSPICR_NSS) // NSS=0
+	// Send command and params
+	d.SpiTx(cmd)
+	for i := 0; i < len(buf); i++ {
+		d.SpiTx(buf[i])
+	}
+	stm32.PWR.SUBGHZSPICR.SetBits(stm32.PWR_SUBGHZSPICR_NSS) // NSS=1
 	if cmd != SX126X_CMD_SET_SLEEP {
 		d.WaitBusy()
 	}
@@ -303,19 +365,46 @@ func (d *Device) ExecSetCommand(cmd uint8, buf []uint8) {
 
 // ExecGetCommand queries the peripheral the peripheral
 func (d *Device) ExecGetCommand(cmd uint8, size int) []uint8 {
-	var buf []uint8
 	d.CheckDeviceReady()
-	stm32.PWR.SUBGHZSPICR.ClearBits(stm32.PWR_SUBGHZSPICR_NSS)
-
-	d.spi.Tx([]byte{cmd}, nil)
-	d.spi.Tx([]byte{0x00}, nil) // Drop first status byte
-	var value [1]byte
+	stm32.PWR.SUBGHZSPICR.ClearBits(stm32.PWR_SUBGHZSPICR_NSS) // NSS=0
+	// Send the command and flush first status byte (as not used)
+	d.SpiTx(cmd)
+	d.SpiTx(0x00)
+	var ret []uint8
+	// Receive given number of bytes from radio
 	for i := 0; i < size; i++ {
-		d.spi.Tx(nil, value[:])
-		buf = append(buf, value[0]) // Read bytes
+		ret = append(ret, d.SpiTx(0xFF))
 	}
-
-	stm32.PWR.SUBGHZSPICR.SetBits(stm32.PWR_SUBGHZSPICR_NSS)
+	stm32.PWR.SUBGHZSPICR.SetBits(stm32.PWR_SUBGHZSPICR_NSS) // NSS=1
 	d.WaitBusy()
-	return buf
+	return ret
+}
+
+//*********************************
+// STM3Cube like SPI communications
+//*********************************
+
+//SpiTx Transmit byte over SPI
+func (d *Device) SpiTx(cmd uint8) uint8 {
+	// Wait until Tx Buffer empty
+	for !stm32.SPI3.SR.HasBits(stm32.SPI_SR_TXE) {
+	}
+	// Write to data register
+	stm32.SPI3.DR.Set(uint32(cmd))
+	// Wait for Rx Data
+	for !stm32.SPI3.SR.HasBits(stm32.SPI_SR_RXNE) {
+	}
+	// Flush RX data
+	ret := uint8(stm32.SPI3.DR.Get())
+	println("SPITX > ", dec2hex(cmd), "<", dec2hex(ret))
+	return ret
+}
+
+//SpiRx Receive byte over SPI
+func (d *Device) SpiRx() uint8 {
+	return d.SpiTx(0xFF)
+}
+
+func dec2hex(dec uint8) string {
+	return fmt.Sprintf("%02x", dec)
 }
